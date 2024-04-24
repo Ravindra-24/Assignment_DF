13e289c4ebd984e9333a806b35d0f69c5a91cc72
Ravindra Pawar <ravindra.pawar.mit@gmail.com>
2024-04-24 14:56:44 +0530

    CORS error solved

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8080;
 connectDB();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://assignment-df.onrender.com",
     credentials: true,
   })
 );
